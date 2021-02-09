4b95138391781b3a53c290b65858ccffda06dc65
Craig McKeachie <craigdmckeachie@gmail.com>
2021-02-08 19:05:27 -0500

    lab06 checkout component-currency not working yet

--- Checkout.js
+++ Checkout.js
@@ -6,8 +6,10 @@ import {
   Text,
   TextInput,
   Button,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
+  StyleSheet
 } from "react-native";
+import * as cart from "./assets/cart.json";
 
 export function Checkout(props) {
   const [firstName, setFirstName] = useState(props.firstName);
@@ -15,6 +17,9 @@ export function Checkout(props) {
   const [creditCard, setCreditCard] = useState(props.creditCard);
   const [email, setEmail] = useState(props.email);
   const [phone, setPhone] = useState(props.phone);
+  const subtotal = cart.seats.reduce((total, seat) => (total += seat.price), 0);
+  const tax = subtotal * 0.0825;
+  const grandtotal = subtotal + tax;
 
   function purchase() {}
 
@@ -23,6 +28,33 @@ export function Checkout(props) {
       <KeyboardAvoidingView behavior="position">
         <ScrollView>
           <Text>Checkout</Text>
+
+          <Text>Your cart</Text>
+          <View style={styles.cart}>
+            {cart.seats.map(seat => (
+              <View style={styles.row} key={seat._id}>
+                <Text>
+                  Table {seat.table_number}, Seat {seat.seat_number}
+                </Text>
+                <Text>{seat.price}</Text>
+              </View>
+            ))}
+          </View>
+          <View style={styles.underline}></View>
+          <View style={styles.row}>
+            <Text>Subtotal</Text>
+            <Text>{subtotal}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text>Tax</Text>
+            <Text>{tax}</Text>
+          </View>
+          <View style={styles.underline}></View>
+          <View style={styles.row}>
+            <Text>Total</Text>
+            <Text>{grandtotal}</Text>
+          </View>
+
           <Text>First name</Text>
           <TextInput value={firstName} onChangeText={setFirstName} />
           <Text>Last name</Text>
@@ -51,3 +83,17 @@ export function Checkout(props) {
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  cart: {
+    width: 350
+  },
+  underline: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1
+  }
+});
